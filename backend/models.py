ff70b519605cf72887201d90fe57be102db0beee
orakomer33-jpg <orak.omer33@gmail.com>
2026-06-29 20:06:46 +0300

    models.py

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,21 @@
+from dataclasses import dataclass
 
+
+@dataclass
+class Operation:
+
+    machine: str
+
+    duration: int
+
+
+@dataclass
+class ScheduledOperation:
+
+    part: str
+
+    machine: str
+
+    start: int
+
+    finish: int
