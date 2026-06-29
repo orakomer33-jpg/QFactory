2c37f87b9c763e0497d55b0e932bb257803c3303
orakomer33-jpg <orak.omer33@gmail.com>
2026-06-29 20:07:02 +0300

    app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,26 @@
+from data import MACHINES
+from data import PARTS
+from data import PROCESS_PLAN
 
+
+print("========== QFactory ==========")
+
+print()
+
+print("Factory Machines")
+
+print(MACHINES)
+
+print()
+
+print("Aircraft Parts")
+
+print(PARTS)
+
+print()
+
+print("Process Plans")
+
+for part, operations in PROCESS_PLAN.items():
+
+    print(part, operations)
