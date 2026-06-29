df08f36e3f8934d4ace3ebb15e4c30a7908bef2b
orakomer33-jpg <orak.omer33@gmail.com>
2026-06-29 20:18:00 +0300

    app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from gantt import create_gantt
 scheduler = FIFOScheduler()
 
 schedule = scheduler.schedule()
-
+stats = scheduler.calculate_statistics(schedule)
 print()
 
 print("===== SCHEDULE =====")
@@ -19,3 +19,34 @@ print("===== MACHINE TIMES =====")
 
 print(scheduler.calculate_utilization())
 create_gantt(schedule)
+print()
+
+print("========== KPI ==========")
+
+print()
+
+print("Makespan")
+
+print(stats["makespan"], "minutes")
+
+print()
+
+print("Bottleneck")
+
+print(stats["bottleneck"])
+
+print()
+
+print("Machine Utilization")
+
+for machine, value in stats["utilization"].items():
+
+    print(machine, ":", value, "%")
+
+print()
+
+print("Idle Time")
+
+for machine, value in stats["idle"].items():
+
+    print(machine, ":", value, "minutes")
