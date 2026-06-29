5d4887e29fdbd9c82ff00ac2c27200c4b04d076e
orakomer33-jpg <orak.omer33@gmail.com>
2026-06-29 20:05:42 +0300

    data.py

--- backend/data.py
+++ backend/data.py
@@ -1,1 +1,17 @@
+"""
+QFactory
+Aircraft Production Planning Data
+"""
 
+# Fabrikadaki makineler
+
+MACHINES = {
+    "CNC": 2,
+    "DRILL": 2,
+    "REAMING": 1,
+    "BORING": 1,
+    "DEBURRING": 2,
+    "CLEANING": 1,
+    "RIVETING": 2,
+    "CMM": 1
+}
