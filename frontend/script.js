a7b1fd2339db39a2a981ab579bd06ce9813a8964
orakomer33-jpg <orak.omer33@gmail.com>
2026-06-29 20:23:15 +0300

    script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,69 @@
+const parts=[
 
+"Wing Bracket",
+
+"Frame Support",
+
+"Spar Joint",
+
+"Stringer Bracket",
+
+"Bulkhead Plate",
+
+"Seat Rail Support"
+
+]
+
+const machines=[
+
+"CNC-1",
+
+"CNC-2",
+
+"DRILL-1",
+
+"DRILL-2",
+
+"REAMING",
+
+"BORING",
+
+"DEBURRING",
+
+"CLEANING",
+
+"RIVETING-1",
+
+"RIVETING-2",
+
+"CMM"
+
+]
+
+const orderDiv=document.getElementById("orders")
+
+parts.forEach(part=>{
+
+const card=document.createElement("div")
+
+card.innerHTML=part
+
+card.className="card"
+
+orderDiv.appendChild(card)
+
+})
+
+const machineDiv=document.getElementById("machines")
+
+machines.forEach(machine=>{
+
+const card=document.createElement("div")
+
+card.innerHTML="🟢 "+machine
+
+card.className="machine"
+
+machineDiv.appendChild(card)
+
+})
