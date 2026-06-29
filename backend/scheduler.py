d0896d102ef7271947da6ce235791e938b3adab6
orakomer33-jpg <orak.omer33@gmail.com>
2026-06-29 20:09:21 +0300

    scheduler.py

--- backend/scheduler.py
+++ backend/scheduler.py
@@ -1,1 +1,73 @@
+from data import MACHINES
+from data import PROCESS_PLAN
 
+class FIFOScheduler:
+
+    def __init__(self):
+
+        self.machine_available = {}
+
+        # Fabrikadaki her makinenin her fiziksel kopyasını oluştur
+        for machine_type, count in MACHINES.items():
+
+            self.machine_available[machine_type] = []
+
+            for i in range(count):
+
+                self.machine_available[machine_type].append({
+                    "name": f"{machine_type}-{i+1}",
+                    "available": 0
+                })
+
+
+    def get_available_machine(self, machine_type):
+
+        machines = self.machine_available[machine_type]
+
+        return min(machines, key=lambda x: x["available"])
+
+    def schedule(self):
+
+        schedule = []
+
+        part_finish_time = {}
+
+        for part in PROCESS_PLAN.keys():
+
+            part_finish_time[part] = 0
+
+        for part, operations in PROCESS_PLAN.items():
+
+            for machine_type, duration in operations:
+
+                machine = self.get_available_machine(machine_type)
+
+                start = max(
+
+                    part_finish_time[part],
+
+                    machine["available"]
+
+                )
+
+                finish = start + duration
+
+                schedule.append({
+
+                    "part": part,
+
+                    "machine": machine["name"],
+
+                    "operation": machine_type,
+
+                    "start": start,
+
+                    "finish": finish
+
+                })
+
+                machine["available"] = finish
+
+                part_finish_time[part] = finish
+
+        return schedule
